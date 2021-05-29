bcab4378ca220c71a464f0857903bf27d707fbdd
Asuma-Samua <asuma.business2@gmail.com>
2021-05-29 13:07:06 +0900

    add template

---
+++ README.txt
@@ -0,0 +1,11 @@
+Web制作時
+npm run watch:all
+▲EJSをHTMLに変換、SASSをCSS(Autoprefix追加)、JavaScriptをコピー、画像を圧縮、Browser-Syncを起動
+
+コード整形時
+npm run format:all
+▲SCSSとJavaScriptの整形
+
+公開前
+npm run prepare:all
+▲main.jsとstyle.cssを圧縮し、SouceMapファイルを削除(実行時のみ)
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const webpack = require('webpack');
+const path = require('path');
+
+module.exports = {
+  // モード値を production に設定すると最適化された状態で、
+  // development に設定するとソースマップ有効でJSファイルが出力される
+  mode: 'development',
+
+  // メインとなるJavaScriptファイル（エントリーポイント）
+  entry: {
+    script: `./src/assets/js/script.js`,
+  },
+  output: {
+    filename: '[name].js',
+    path: path.join(__dirname, 'dist/assets/js')
+  },
+  module: {
+    rules: [
+      {
+        // 拡張子 .js の場合
+        test: /\.js$/,
+        use: [
+          {
+            // Babel を利用する
+            loader: 'babel-loader',
+            // Babel のオプションを指定する
+            options: {
+              presets: [
+                // プリセットを指定することで、ES2019 を ES5 に変換
+                '@babel/preset-env',
+              ],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'source-map',
+};
